77cdcf83a58e562df9463d6345b17f3c0acb2a21
Eugen Guriev <gurievcreative@gmail.com>
2025-02-06 21:37:58 +0200

    refactor: integrate useMeQuery and useUpdateMetaMutate hooks

--- src/wizard/five/page.tsx
+++ src/wizard/five/page.tsx
@@ -12,20 +12,37 @@ import {
 } from "@/components/ui/form";
 import EnterHint from "../components/enter-hint";
 import { useNavigate } from "react-router";
+import useMeQuery from "@/hooks/useMeQuery";
+import useUpdateMetaMutate from "@/hooks/useUpdateMetaMutate";
 
 const FivePage = () => {
+  const { data } = useMeQuery();
+  const { mutate, isPending } = useUpdateMetaMutate({
+    queryOptions: {
+      onSuccess: () => {
+        navigate("/wizard/six");
+      },
+    },
+  });
   const navigate = useNavigate();
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     mode: "onBlur",
     defaultValues: {
-      weight: undefined,
+      weight: data.meta?.weight,
     },
   });
 
   const handleSubmit = async (body: FormSchema) => {
-    console.log("log: submit", body);
-    navigate("/wizard/six");
+    mutate({
+      headers: { "Content-type": "application/json" },
+      body: {
+        meta: {
+          ...(data.meta || {}),
+          ...body,
+        },
+      },
+    });
   };
 
   return (
@@ -47,6 +64,7 @@ const FivePage = () => {
                       className="sm:w-80 w-full border-none shadow-none focus-visible:ring-0"
                       placeholder="Ваша вага (кг)"
                       type="number"
+                      disabled={isPending}
                       {...field}
                     />
                   </div>
@@ -56,7 +74,9 @@ const FivePage = () => {
             )}
           />
           <div className="flex flex-col gap-1 px-3 w-full sm:w-auto">
-            <Button disabled={!form.formState.isValid}>Продовжуйте</Button>
+            <Button disabled={!form.formState.isValid || isPending}>
+              Продовжуйте
+            </Button>
             <EnterHint valid={form.formState.isValid} />
           </div>
         </form>
