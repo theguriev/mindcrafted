9e6a098d9323da5d0ef12d1ef8b30065143f9056
Eugen Guriev <gurievcreative@gmail.com>
2025-02-09 10:58:14 +0200

    refactor: replace form handling in FifteenPage with WizardForm component

--- src/wizard/fifteen/page.tsx
+++ src/wizard/fifteen/page.tsx
@@ -1,66 +1,48 @@
-import { Button } from "@/components/ui/button";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { FormSchema, formSchema } from "./zod";
+import { formSchema } from "./zod";
 import {
   FormField,
   FormItem,
   FormControl,
   FormMessage,
-  Form,
 } from "@/components/ui/form";
-import EnterHint from "../components/enter-hint";
-import { useNavigate } from "react-router";
 import { Textarea } from "@/components/ui/textarea";
+import WizardForm from "../components/wizard-form";
+import WizardFormFooter from "../components/wizard-form-footer";
+import useWizardStep from "../hooks/useWizardStep";
 
 const FifteenPage = () => {
-  const navigate = useNavigate();
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    mode: "onBlur",
-    defaultValues: {
-      physicalActivity: undefined,
-    },
+  const { form, handleSubmit, isPending } = useWizardStep({
+    to: "/wizard/sixteen",
+    getDefaultValues: (data) => ({
+      physicalActivity: data.meta?.physicalActivity,
+    }),
+    prepareBody: (body) => body,
+    formSchema,
   });
 
-  const handleSubmit = async (body: FormSchema) => {
-    console.log("log: submit", body);
-    navigate("/wizard/sixteen");
-  };
-
   return (
-    <div className="min-h-screen flex items-center bg-white">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleSubmit)}
-          className="flex flex-col w-full gap-4 sm:flex-row sm:items-start sm:justify-center"
-        >
-          <FormField
-            control={form.control}
-            name="physicalActivity"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="w-full sm:w-auto flex items-center">
-                    <Textarea
-                      autoFocus
-                      className="sm:w-80 w-full border-none shadow-none focus-visible:ring-0"
-                      placeholder="Яка у вас рухова активність за останній рік, включно з тренуваннями і роботою?"
-                      {...field}
-                    />
-                  </div>
-                </FormControl>
-                <FormMessage className="px-3" />
-              </FormItem>
-            )}
-          />
-          <div className="flex flex-col gap-1 px-3 w-full sm:w-auto">
-            <Button disabled={!form.formState.isValid}>Продовжуйте</Button>
-            <EnterHint valid={form.formState.isValid} />
-          </div>
-        </form>
-      </Form>
-    </div>
+    <WizardForm onSubmit={form.handleSubmit(handleSubmit)} {...form}>
+      <FormField
+        control={form.control}
+        name="physicalActivity"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <div className="w-full sm:w-auto flex items-center">
+                <Textarea
+                  autoFocus
+                  className="sm:w-80 w-full border-none shadow-none focus-visible:ring-0"
+                  placeholder="Яка у вас рухова активність за останній рік, включно з тренуваннями і роботою?"
+                  {...field}
+                />
+              </div>
+            </FormControl>
+            <FormMessage className="px-3" />
+          </FormItem>
+        )}
+      />
+      <WizardFormFooter valid={form.formState.isValid} pending={isPending} />
+    </WizardForm>
   );
 };
 
