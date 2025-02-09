86d9dfa41b6f39a423de3c49d87726ab48b3fb61
Eugen Guriev <gurievcreative@gmail.com>
2025-02-09 10:42:02 +0200

    refactor: replace form handling in EightPage with WizardForm component

--- src/wizard/eight/page.tsx
+++ src/wizard/eight/page.tsx
@@ -1,15 +1,14 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   FormField,
   FormItem,
   FormControl,
   FormMessage,
-  Form,
 } from "@/components/ui/form";
-import EnterHint from "../components/enter-hint";
 import useWizardStep from "../hooks/useWizardStep";
 import { formSchema } from "./zod";
+import WizardForm from "../components/wizard-form";
+import WizardFormFooter from "../components/wizard-form-footer";
 
 const EightPage = () => {
   const { form, handleSubmit, isPending } = useWizardStep({
@@ -22,42 +21,30 @@ const EightPage = () => {
   });
 
   return (
-    <div className="min-h-screen flex items-center bg-white">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleSubmit)}
-          className="flex flex-col w-full gap-4 sm:flex-row sm:items-start sm:justify-center"
-        >
-          <FormField
-            control={form.control}
-            name="hipMeasurement"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="w-full sm:w-auto flex items-center">
-                    <Input
-                      autoFocus
-                      className="sm:w-80 w-full border-none shadow-none focus-visible:ring-0"
-                      placeholder="Обхват стегна (сідниці см)"
-                      type="number"
-                      disabled={isPending}
-                      {...field}
-                    />
-                  </div>
-                </FormControl>
-                <FormMessage className="px-3" />
-              </FormItem>
-            )}
-          />
-          <div className="flex flex-col gap-1 px-3 w-full sm:w-auto">
-            <Button disabled={!form.formState.isValid || isPending}>
-              Продовжуйте
-            </Button>
-            <EnterHint valid={form.formState.isValid} />
-          </div>
-        </form>
-      </Form>
-    </div>
+    <WizardForm onSubmit={form.handleSubmit(handleSubmit)} {...form}>
+      <FormField
+        control={form.control}
+        name="hipMeasurement"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <div className="w-full sm:w-auto flex items-center">
+                <Input
+                  autoFocus
+                  className="sm:w-80 w-full border-none shadow-none focus-visible:ring-0"
+                  placeholder="Обхват стегна (сідниці см)"
+                  type="number"
+                  disabled={isPending}
+                  {...field}
+                />
+              </div>
+            </FormControl>
+            <FormMessage className="px-3" />
+          </FormItem>
+        )}
+      />
+      <WizardFormFooter valid={form.formState.isValid} pending={isPending} />
+    </WizardForm>
   );
 };
 
