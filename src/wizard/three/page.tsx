25f064f3f8289f323c3bac8712902cbb32c5af54
Eugen Guriev <gurievcreative@gmail.com>
2025-02-09 10:32:36 +0200

    refactor: replace form handling in ThreePage with WizardForm component

--- src/wizard/three/page.tsx
+++ src/wizard/three/page.tsx
@@ -4,9 +4,7 @@ import {
   FormItem,
   FormControl,
   FormMessage,
-  Form,
 } from "@/components/ui/form";
-import EnterHint from "../components/enter-hint";
 import { cn } from "@/lib/utils";
 import { CalendarIcon } from "lucide-react";
 import { format } from "date-fns";
@@ -18,6 +16,7 @@ import {
 import { Calendar } from "@/components/ui/calendar";
 import useWizardStep from "../hooks/useWizardStep";
 import { formSchema } from "./zod";
+import WizardForm from "../components/wizard-form";
 
 const ThreePage = () => {
   const { form, handleSubmit, isPending } = useWizardStep({
@@ -32,63 +31,51 @@ const ThreePage = () => {
   });
 
   return (
-    <div className="min-h-screen flex items-center bg-white">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleSubmit)}
-          className="flex flex-col w-full gap-4 sm:flex-row sm:items-start sm:justify-center"
-        >
-          <FormField
-            control={form.control}
-            name="birthday"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="w-full px-3 sm:w-auto flex items-center">
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={"outline"}
-                          className={cn(
-                            "w-full sm:w-80 justify-start text-left font-normal",
-                            !field.value && "text-muted-foreground"
-                          )}
-                        >
-                          <CalendarIcon />
-                          {field.value ? (
-                            format(field.value, "PPP")
-                          ) : (
-                            <span>День вашого народження</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          captionLayout="dropdown"
-                          selected={field.value}
-                          onSelect={(value) => field.onChange(value)}
-                          fromYear={1950}
-                          toYear={new Date().getFullYear()}
-                          defaultMonth={field.value}
-                        />
-                      </PopoverContent>
-                    </Popover>
-                  </div>
-                </FormControl>
-                <FormMessage className="px-3" />
-              </FormItem>
-            )}
-          />
-          <div className="flex flex-col gap-1 px-3 w-full sm:w-auto">
-            <Button disabled={!form.formState.isValid || isPending}>
-              Продовжуйте
-            </Button>
-            <EnterHint valid={form.formState.isValid} />
-          </div>
-        </form>
-      </Form>
-    </div>
+    <WizardForm onSubmit={form.handleSubmit(handleSubmit)} {...form}>
+      <FormField
+        control={form.control}
+        name="birthday"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <div className="w-full px-3 sm:w-auto flex items-center">
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-full sm:w-80 justify-start text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon />
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>День вашого народження</span>
+                      )}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      captionLayout="dropdown"
+                      selected={field.value}
+                      onSelect={(value) => field.onChange(value)}
+                      fromYear={1950}
+                      toYear={new Date().getFullYear()}
+                      defaultMonth={field.value}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </FormControl>
+            <FormMessage className="px-3" />
+          </FormItem>
+        )}
+      />
+      <WizardFormFooter valid={form.formState.isValid} pending={isPending} />
+    </WizardForm>
   );
 };
 
