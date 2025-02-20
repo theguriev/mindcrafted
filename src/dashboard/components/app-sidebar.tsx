221a1f033fb63a77b5fe1732399d7eae1b0f57f2
Eugen Guriev <gurievcreative@gmail.com>
2025-02-20 19:17:38 +0200

    feat: update AppSidebar with new branding and gradient background

--- src/dashboard/components/app-sidebar.tsx
+++ src/dashboard/components/app-sidebar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import {
-  Command,
   Inbox,
   LayoutDashboard,
   Utensils,
@@ -90,12 +89,14 @@ const AppSidebar = ({ ...props }: React.ComponentProps<typeof Sidebar>) => {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
               <a href="#">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Command className="size-4" />
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-sidebar-primary-foreground">
+                  <Dumbbell className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">Acme Inc</span>
-                  <span className="truncate text-xs">Enterprise</span>
+                  <span className="truncate font-semibold">cmnsns.in</span>
+                  <span className="truncate text-xs">
+                    Наука та здоровий глузд
+                  </span>
                 </div>
               </a>
             </SidebarMenuButton>
