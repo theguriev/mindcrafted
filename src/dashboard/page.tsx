55b02ddba334f34e1fe36333ed501b012f66824e
Eugen Guriev <gurievcreative@gmail.com>
2025-02-22 21:19:00 +0200

    feat: wrap Card component in Link for navigation to steps page

--- src/dashboard/page.tsx
+++ src/dashboard/page.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/sidebar";
 import type React from "react";
 import AppSidebar from "./components/app-sidebar";
+import { Link } from "react-router";
 // import { DashboardMetrics } from "./components/dashboard-metrics";
 import { Card, CardContent } from "@/components/ui/card";
 import CircularProgress from "./components/circular-progress";
@@ -28,24 +29,26 @@ const DashboardPage: React.FC = () => {
           <h1 className="text-xl font-semibold">Головна</h1>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <Card className="relative overflow-hidden transition-all hover:shadow-lg">
-            <CardContent className="p-6">
-              <div className="flex items-center justify-between">
-                <div className="space-y-2">
-                  <h2 className="text-2xl font-bold">
-                    {todaySteps.toLocaleString()}
-                  </h2>
-                  <p className="text-sm text-muted-foreground">
-                    Кроків сьогодні
-                  </p>
-                  <div className="text-sm text-muted-foreground">
-                    Ціль: {goalSteps.toLocaleString()} кроків
+          <Link to="/dashboard/steps">
+            <Card className="relative overflow-hidden transition-all hover:shadow-lg">
+              <CardContent className="p-6">
+                <div className="flex items-center justify-between">
+                  <div className="space-y-2">
+                    <h2 className="text-2xl font-bold">
+                      {todaySteps.toLocaleString()}
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                      Кроків сьогодні
+                    </p>
+                    <div className="text-sm text-muted-foreground">
+                      Ціль: {goalSteps.toLocaleString()} кроків
+                    </div>
                   </div>
+                  <CircularProgress value={progress} />
                 </div>
-                <CircularProgress value={progress} />
-              </div>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          </Link>
           {/* <DashboardMetrics /> */}
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             {Array.from({ length: 6 }).map((_, index) => (
