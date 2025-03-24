65fc23266f6887c793eebd33bd8209a443f49709
Eugen Guriev <gurievcreative@gmail.com>
2025-03-24 21:39:19 +0200

    feat: refactor WizardRoutes component and remove unused useWizardStep hook

--- src/wizard-routes.tsx
+++ src/wizard-routes.tsx
@@ -1,43 +1,18 @@
-import { Route, Routes, useNavigate } from "react-router";
+import { Route, Routes } from "react-router";
 import { FC } from "react";
 import PrivateBoundary from "./components/private-boundary";
-import useUpdateMetaMutate from "./hooks/useUpdateMetaMutate";
-import { FieldValues } from "react-hook-form";
 import routes from "./constants/routes";
 
 const WizardRoutes: FC = () => {
-  const navigate = useNavigate();
-  const { mutate, isPending } = useUpdateMetaMutate();
-
-  const createSubmitHandle =
-    (index: number) => (body: { meta: FieldValues }) => {
-      const routesList =
-        body.meta.sex === "male" ? routes.slice(0, routes.length - 2) : routes;
-      const path = routesList[index + 1]?.[0] || "/welcome";
-      mutate(
-        {
-          headers: { "Content-type": "application/json" },
-          body,
-        },
-        {
-          onSuccess: () => {
-            navigate(path);
-          },
-        }
-      );
-    };
   return (
     <Routes>
-      {routes.map(([path, Component], index) => (
+      {routes.map(([path, Component]) => (
         <Route
-          key={path}
-          path={path}
+          key={String(path)}
+          path={String(path)}
           element={
             <PrivateBoundary>
-              <Component
-                pending={isPending}
-                onSubmit={createSubmitHandle(index)}
-              />
+              <Component />
             </PrivateBoundary>
           }
         />
